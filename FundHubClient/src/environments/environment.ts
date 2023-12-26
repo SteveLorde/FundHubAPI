2077ec5d53fc51cb22a108f66ea357a619d2c530
SteveLorde <mostafa.maher98@gmail.com>
2023-12-26 17:26:46 +0200

    fixed frontend api env url

--- FundHubClient/src/environments/environment.ts
+++ FundHubClient/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backendurl: "https://e-shopapi-730825c33287.herokuapp.com/"
+  backendurl: "https://e-shopapi-730825c33287.herokuapp.com"
 }
 
 export default environment
