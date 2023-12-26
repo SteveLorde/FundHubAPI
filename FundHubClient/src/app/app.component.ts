07c159f1039385721f8d21ddea9254dd603ca008
SteveLorde <mostafa.maher98@gmail.com>
2023-12-26 22:15:48 +0200

    footerbar, some styling update

--- FundHubClient/src/app/app.component.ts
+++ FundHubClient/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./Components/Navbar/navbar.component";
+import {FooterbarComponent} from "./Components/Footerbar/footerbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
